c0aba4d2eafbcfcc5cc1e2495003cdd404d791b0
Colton Bassett <coltonbassett@gmail.com>
2024-11-18 11:51:24 -0800

    Added links to footer and nav. Added github link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,12 +7,22 @@ export default function Footer() {
   return (
     <Box className={styles.footerContainer}>
       <footer className={styles.footer}>
-        <Box>
+        <Box className={styles.linkContainer}>
           <a href="/" className={styles.logoLink}>
             <img src={reactLogo} className={styles.logo} alt="React logo" />
 
             <Typography variant="h6" className={styles.logoText}>
               Pokedex
+            </Typography>
+          </a>
+          <a href="/help" className={styles.link}>
+            <Typography variant="h6" className={styles.linkText}>
+              Help
+            </Typography>
+          </a>
+          <a href="/disclaimer" className={styles.link}>
+            <Typography variant="h6" className={styles.linkText}>
+              Disclaimer
             </Typography>
           </a>
         </Box>
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,4 +1,5 @@
 import reactLogo from "../assets/react_logo_dark.svg";
+import githubLogo from "../assets/github_logo_dark.svg";
 
 import { Box, Typography } from "@mui/material";
 import styles from "./nav.module.css";
@@ -7,13 +8,29 @@ export default function Nav() {
     <>
       <Box className={styles.container}>
         <Box component="nav" className={styles.nav}>
-          <a href="/" className={styles.logoLink}>
+          <a href="/" className={styles.link}>
             <img src={reactLogo} className={styles.logo} alt="React logo" />
 
             <Typography variant="h6" className={styles.logoText}>
               Pokedex
             </Typography>
           </a>
+          <Box className={styles.linkContainer}>
+            <a href="help" className={styles.link}>
+              <Typography className={styles.linkText}>Help</Typography>
+            </a>
+            <a href="/disclaimer" className={styles.link}>
+              <Typography className={styles.linkText}>Disclaimer</Typography>
+            </a>
+            <a
+              href="https://github.com/colton-bassett/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.githubLink}
+            >
+              <img src={githubLogo} alt="Github website" />
+            </a>
+          </Box>
         </Box>
       </Box>
     </>
